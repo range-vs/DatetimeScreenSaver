cfce9bf4aa6b36231f5266e342bd52cc76d4cc0d
range-vs <trubnicov_ivan1996@mail.ru>
2024-04-26 00:02:00 +0300

    + add support weather draw

--- datetime_screensaver/MainWindow.xaml.cs
+++ datetime_screensaver/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,18 +14,26 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 using System.Windows.Threading;
+using System.IO;
 
 namespace datetime_screensaver
 {
     public partial class MainWindow : Window
     {
+        private static string FileName = "appconfig.json";
+        private static string WeatherApiKey = "";
+        private static string CityKey = "";
+
         public DispatcherTimer Timer { get; set; }
         public bool IsTimeSplitter { get; set; } = true;
         public DispatcherTimer TimerSplitter { get; set; }
         private DateTime LastSystemTime { get; set; }
         public DispatcherTimer TimerChangeColor { get; set; }
         public bool ModeColor { get; set; } = true;
+        public DispatcherTimer WeatherTimer { get; set; }
+
 
         public MainWindow()
         {
@@ -34,6 +43,21 @@ namespace datetime_screensaver
 
         public void Init()
         {
+            try
+            {
+                using (StreamReader sr = new StreamReader(FileName))
+                {
+                    string fileContent = sr.ReadToEnd();
+                    dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(fileContent);
+                    WeatherApiKey = data.WEATHER_API_KEY;
+                    CityKey = data.CITY_KEY;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ошибка получения ключей погоды");
+            }
+
             Mouse.OverrideCursor = Cursors.None;
 
             WindowState = WindowState.Maximized; // Установка окна в состояние "Максимизировано"
@@ -48,6 +72,12 @@ namespace datetime_screensaver
             TimerChangeColor.Tick += new EventHandler(ChangeColor);
             TimerChangeColor.Interval = TimeSpan.FromMinutes(20);
             TimerChangeColor.Start();
+
+            WeatherTimer = new DispatcherTimer();
+            WeatherTimer.Tick += new EventHandler(UpdateWeather);
+            WeatherTimer.Interval = TimeSpan.FromMinutes(5);
+            WeatherTimer.Start();
+            UpdateWeather(null, null);
 
             KeyDown += KeyDownHandler;
             MouseWheel += MouseWheelHandler;
@@ -106,6 +136,43 @@ namespace datetime_screensaver
             //IsTimeSplitter = !IsTimeSplitter;
         }
 
+        private void UpdateWeather(object sender, EventArgs e)
+        {
+            string apiKey = "1e93f3de28da4dec987231925242204";
+            string city = "Воронеж";
+            string url = $"http://api.weatherapi.com/v1/current.json?key={apiKey}&q={city}&lang=ru";//$"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&lang=ru";
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = client.GetAsync(url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = response.Content.ReadAsStringAsync().Result;
+                    dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+
+                    string weatherDescription = data.current.condition.text;
+                    double temperature = data.current.temp_c;
+                    int humidity = data.current.humidity;
+
+                    string _out = string.Format($"Погода в {city}: {weatherDescription}\nТемпература: {temperature}°C\nВлажность: {humidity}%\n");
+                    Console.WriteLine(_out);
+                    Weather.Text = _out;
+                }
+                else
+                {
+                    string _out = $"Ошибка получения погоды: {response.ReasonPhrase}";
+                    Console.WriteLine(_out);
+                    Weather.Text = _out;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ошибка получения погоды");
+            }
+        }
+
         private void ChangeColor(object sender, EventArgs e)
         {
             ModeColor = !ModeColor;
@@ -113,12 +180,14 @@ namespace datetime_screensaver
             {
                 Date.Foreground = new SolidColorBrush(Colors.White);
                 Time.Foreground = new SolidColorBrush(Colors.White);
+                Weather.Foreground = new SolidColorBrush(Colors.White);
                 Window.Background = new SolidColorBrush(Colors.Black);
             }
             else
             {
                 Date.Foreground = new SolidColorBrush(Colors.Gold);
                 Time.Foreground = new SolidColorBrush(Colors.Gold);
+                Weather.Foreground = new SolidColorBrush(Colors.Gold);
                 Window.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF0D4054"));
             }
         }
